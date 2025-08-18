198b9ee3be162d72010b53e8bdf726c4506314fe
Daniel Pererva <156557819+NachoManFTW@users.noreply.github.com>
2025-08-18 13:12:27 -0500

    Added Bubble sort
    Create a bubble sort for integer vectors

--- SortingAlgorithms/src/main.cpp
+++ SortingAlgorithms/src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include "comparison/bubbleSort.cpp"
 
-
+using namespace std;
 int main()
 {
 
+	//Create random
+	random_device rd;
+	mt19937_64 gen(rd());//Use gen mt19937_64
+	uniform_int_distribution<> distrib(1, 100);
+
+	vector<int> listOfNumbers(20000);
+
+	for (int& val : listOfNumbers)
+	{
+		val = distrib(gen);
+	}
+
+	BubbleSort(listOfNumbers);
 }
